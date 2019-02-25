392ca4642ce8d270b38017a389642864bf38f55a
Zhongyuan Hau <zh1316@imperial.ac.uk>
2019-02-25 15:20:34 +0000

    initial commit

---
+++ src/modules/test_url.py
@@ -0,0 +1,23 @@
+from url_parse import *
+import sys
+
+def main():
+    shortenedUrl = sys.argv[1]
+    print('shortened url : ' + shortenedUrl)
+    match1 = check_google_safebrowsing(shortenedUrl)
+    check_json(match1)
+    fullUrl = unshorten_url(shortenedUrl)
+    print('full url : ' + fullUrl)
+    match2 = check_google_safebrowsing(fullUrl)
+    check_json(match2)
+
+
+if __name__ == "__main__":
+    main()
+
+'''
+Instruction :
+
+$ python3 test_url.py <url here>
+
+''' 
---
+++ src/modules/url_parse.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+import requests
+import os
+import json
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+EMPTY  = 0
+DATA = 1
+
+def get_url_params(url) :
+    r = requests.get(url, allow_redirects=False)
+    r.raise_for_status()
+    return (r.status_code, r.headers['Location'])
+
+def unshorten_url(url) :
+    statusCode, link  = get_url_params(url)
+    while statusCode/100 == 3 :
+        statusCode, link = get_url_params(url)
+        url = link
+    return link
+
+def get_headers(url) :
+    r = requests.get(url, allow_redirects=False)
+    print(r.headers)
+    return
+
+def check_google_safebrowsing (url) :
+    google_url = 'https://safebrowsing.googleapis.com/v4/threatMatches:find?key='+str(GOOGLE_API_KEY)
+    payload = {"client" : {
+      "clientId" : "Check-URL" ,
+      "clientVersion" : "1.5.2"} ,
+      "threatInfo" : {
+      "threatTypes" :      ["MALWARE", "SOCIAL_ENGINEERING"],
+      "platformTypes" :    ["ANY_PLATFORM"],
+      "threatEntryTypes" : ["URL"],
+      "threatEntries" : [
+        {"url" : str(url)}
+        ]
+      }
+     }
+    header = {'Content-Type' : 'application/json'}
+    r = requests.post(google_url, data = json.dumps(payload), headers = header)
+    r.raise_for_status()
+    if r.status_code == 200:
+        response_data = json.loads(r.text)
+        return response_data
+    return json.dumps('{}')
+
+def check_json (data) :
+    if data == json.loads('{}') :
+        print('NO MATCH WITH GOOGLE BLACKLIST')
+        return EMPTY
+    print(data)
+    return DATA
