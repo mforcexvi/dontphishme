472f5cc2e5f5c03439dde0f05955662790af7228
Zhongyuan Hau <zh1316@imperial.ac.uk>
2019-02-25 15:48:51 +0000

    check_json_google returns threattype in str

--- src/modules/test_url.py
+++ src/modules/test_url.py
@@ -5,11 +5,11 @@ def main():
     shortenedUrl = sys.argv[1]
     print('shortened url : ' + shortenedUrl)
     match1 = check_google_safebrowsing(shortenedUrl)
-    check_json(match1)
+    check_json_google(match1)
     fullUrl = unshorten_url(shortenedUrl)
     print('full url : ' + fullUrl)
     match2 = check_google_safebrowsing(fullUrl)
-    check_json(match2)
+    check_json_google(match2)
 
 
 if __name__ == "__main__":
@@ -20,4 +20,4 @@ Instruction :
 
 $ python3 test_url.py <url here>
 
-''' 
+'''
--- src/modules/url_parse.py
+++ src/modules/url_parse.py
@@ -46,9 +46,10 @@ def check_google_safebrowsing (url) :
         return response_data
     return json.dumps('{}')
 
-def check_json (data) :
+def check_json_google (data) :
     if data == json.loads('{}') :
-        print('NO MATCH WITH GOOGLE BLACKLIST')
+        print('NO MATCH WITH GOOGLE BLACKLIST - Probably Safe :P')
         return EMPTY
-    print(data)
-    return DATA
+    # print(data["matches"][-1]["threatType"])
+    result = "Threat Type: " + data["matches"][-1]["threatType"]
+    return result
