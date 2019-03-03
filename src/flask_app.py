427d73adc15d04f3c941f348fc8b356953cc56d5
Matthew <mzw17@ic.ac.uk>
2019-03-03 08:43:26 +0000

    Moved QR code decoding to ocr_api module and call both qr code decoding and ocr from main flask script

--- src/flask_app.py
+++ src/flask_app.py
@@ -5,8 +5,9 @@ from werkzeug.utils import secure_filename
 from PIL import Image
 import math
 import modules.ocr_api as ocr
+import base64
 
-UPLOAD_FOLDER = '/home/nophish/nophishsite/uploads'
+UPLOAD_FOLDER = '/home/nophish/static'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
@@ -35,29 +36,49 @@ def upload_file():
             # return redirect(url_for('uploaded_file', filename=filename))
 
             uploaded_image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            uploaded_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             dimensions = (math.floor((uploaded_image.size[0])/10), math.floor((uploaded_image.size[1])/10))
             resized = uploaded_image.resize(dimensions,Image.ANTIALIAS)
-            resized.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            resized.save(os.path.join(app.config['UPLOAD_FOLDER'], "r"+filename))
 
-            qr_url = ocr.decode_qr(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            ocr_url = ocr.run_ocr('http://nophish.pythonanywhere.com/' + filename)
+            qr_url = ocr.decode_qr(os.path.join(app.config['UPLOAD_FOLDER'], "r"+filename))
+            ocr_url = ocr.run_ocr('http://nophish.pythonanywhere.com/static/' + filename)
+
 
             # results_list = decode(Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
             # result_string = results_list[0][0].decode("utf-8")
 
-            output_string = qr_url
+            # output_string = qr_url
+            # for url in ocr_url:
+            #     if url != None:
+            #         output_string += " "
+            #         output_string += url
+
+            url_list = []
+            if qr_url != "None" and qr_url != None:
+                url_list.append(qr_url)
             for url in ocr_url:
-                output_string += " "
-                output_string += url
+                if url != None:
+                    url_list.append(url)
+
+            output_string = ""
+            if (len(url_list) == 0):
+                output_string = "No urls detected"
+            else:
+                for url in url_list:
+                    output_string += " "
+                    output_string += url
 
             return output_string
-    return
-    '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+
+        return "abc"
+    # return
+    # '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=file>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
--- src/modules/ocr_api.py
+++ src/modules/ocr_api.py
@@ -4,33 +4,49 @@ import re
 from dotenv import load_dotenv
 import os
 from pyzbar.pyzbar import decode
+from PIL import Image
 load_dotenv()
+import time
 
 
 def run_ocr(image_uri):
     headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
+    print(image_uri)
     data_payload = json.loads('{ "requests": [ { "image": { "source": { "imageUri": "%s" } }, "features": [ { "type": "TEXT_DETECTION" } ] } ] } ' % (image_uri))
+    # data_payload = json.loads('{ "requests": [ { "image": { "content": "%s" }, "features": [ { "type": "TEXT_DETECTION" } ] } ] } ' % (image_uri))
+
     KEY = os.getenv("myKey")
     print(KEY)
     # os.go()
     target_url = "https://vision.googleapis.com/v1/images:annotate?key=" + os.getenv("myKey")
+
+    time.sleep(10)
 
     try:
         r = requests.post(target_url, headers=headers, json = data_payload)
         results = r.text
         results = json.loads(results)
         results = results["responses"][0]["textAnnotations"][0]["description"]
-    except KeyError:
+    except:
         # For some reason, the first API call sometimes returns nothing, but when the API is called again everything is fine
         r = requests.post(target_url, headers=headers, json = data_payload)
         results = r.text
         results = json.loads(results)
-        results = results["responses"][0]["textAnnotations"][0]["description"]
+        try:
+            results = results["responses"][0]["textAnnotations"][0]["description"]
+        except:
+            print(results)
+            return([None])
 
     urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', results)
+    print(urls)
     return(urls)
 
 def decode_qr(filepath):
     results_list = decode(Image.open(filepath))
-    result_string = results_list[0][0].decode("utf-8")
-    return result_string
+    print(results_list)
+    try:
+        result_string = results_list[0][0].decode("utf-8")
+        return result_string
+    except:
+        return "None"
